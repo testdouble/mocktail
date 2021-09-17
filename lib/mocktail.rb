26e9ee1b9259b39cf586a66194eaa9ceab4e199d
Justin Searls <searls@gmail.com>
2021-09-17 09:14:51 -0400

    bundle gem mocktail

---
+++ lib/mocktail.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "mocktail/version"
+
+module Mocktail
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/mocktail/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Mocktail
+  VERSION = "0.1.0"
+end

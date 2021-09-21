c7265411ae7fb75d46b2ce1b0747100b148375ee
Justin Searls <searls@gmail.com>
2021-09-21 08:05:55 -0400

    extract a couple privates while thinking about class/module mocking

--- lib/mocktail/imitates_type/makes_double/declares_dry_class.rb
+++ lib/mocktail/imitates_type/makes_double/declares_dry_class.rb
@@ -1,47 +1,62 @@
 module Mocktail
   class DeclaresDryClass
+    def initialize
+      @handles_dry_call = HandlesDryCall.new
+    end
+
     def declare(type)
       type_type = type_of(type)
       instance_methods = instance_methods_on(type)
-      Class.new(type_type == :class ? type : Object) {
+      dry_class = Class.new(type_type == :class ? type : Object) {
         include type if type_type == :module
-
-        [:to_s, :inspect].each do |method_name|
-          define_singleton_method method_name, -> {
-            if (id_matches = super().match(/:([0-9a-fx]+)>$/))
-              "#<Class #{"including module " if type_type == :module}for mocktail of #{type.name}:#{id_matches[1]}>"
-            else
-              super()
-            end
-          }
-
-          next if instance_methods.include?(method_name)
-          define_method method_name, -> {
-            if (id_matches = super().match(/:([0-9a-fx]+)>$/))
-              "#<Mocktail of #{type.name}:#{id_matches[1]}>"
-            else
-              super()
-            end
-          }
-        end
-
-        instance_methods.each do |method|
-          define_method method, ->(*args, **kwargs, &block) {
-            HandlesDryCall.new.handle(Call.new(
-              double: self,
-              original_type: type,
-              dry_type: self.class,
-              method: method,
-              args: args,
-              kwargs: kwargs,
-              block: block
-            ))
-          }
-        end
       }
+
+      add_stringify_methods!(dry_class, :to_s, type, type_type, instance_methods)
+      add_stringify_methods!(dry_class, :inspect, type, type_type, instance_methods)
+
+      define_double_methods!(dry_class, type, instance_methods)
+
+      dry_class
     end
 
     private
+
+    def define_double_methods!(dry_class, type, instance_methods)
+      handles_dry_call = @handles_dry_call
+      instance_methods.each do |method|
+        dry_class.define_method method, ->(*args, **kwargs, &block) {
+          handles_dry_call.handle(Call.new(
+            double: self,
+            original_type: type,
+            dry_type: self.class,
+            method: method,
+            args: args,
+            kwargs: kwargs,
+            block: block
+          ))
+        }
+      end
+    end
+
+    def add_stringify_methods!(dry_class, method_name, type, type_type, instance_methods)
+      dry_class.define_singleton_method method_name, -> {
+        if (id_matches = super().match(/:([0-9a-fx]+)>$/))
+          "#<Class #{"including module " if type_type == :module}for mocktail of #{type.name}:#{id_matches[1]}>"
+        else
+          super()
+        end
+      }
+
+      unless instance_methods.include?(method_name)
+        dry_class.define_method method_name, -> {
+          if (id_matches = super().match(/:([0-9a-fx]+)>$/))
+            "#<Mocktail of #{type.name}:#{id_matches[1]}>"
+          else
+            super()
+          end
+        }
+      end
+    end
 
     def type_of(type)
       if type.is_a?(Class)

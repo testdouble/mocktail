f22905894e033261bace8febf1a399ec084171f0
Justin Searls <searls@gmail.com>
2022-06-10 12:48:59 -0400

    pair with @kbaribeau

--- lib/mocktail/handles_dry_call/fulfills_stubbing/describes_unsatisfied_stubbing.rb
+++ lib/mocktail/handles_dry_call/fulfills_stubbing/describes_unsatisfied_stubbing.rb
@@ -1,17 +1,19 @@
 require_relative "../../share/cleans_backtrace"
+require_relative "../../share/compares_safely"
 
 module Mocktail
   class DescribesUnsatisfiedStubbing
     def initialize
       @cleans_backtrace = CleansBacktrace.new
+      @compares_safely = ComparesSafely.new
     end
 
     def describe(dry_call)
       UnsatisfyingCall.new(
         call: dry_call,
         other_stubbings: Mocktail.cabinet.stubbings.select { |stubbing|
-          dry_call.double == stubbing.recording.double &&
-            dry_call.method == stubbing.recording.method
+          @compares_safely.compare(dry_call.double, stubbing.recording.double) &&
+            @compares_safely.compare(dry_call.method, stubbing.recording.method)
         },
         backtrace: @cleans_backtrace.clean(Error.new).backtrace
       )

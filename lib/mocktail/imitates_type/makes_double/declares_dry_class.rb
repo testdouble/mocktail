1754df4e7adba5a0f2d706e7d792770572dd57da
Caleb Hearth <caleb@calebhearth.com>
2022-06-29 10:57:11 -0500

    WIP - Plug in BuildsMethodSignature

--- lib/mocktail/imitates_type/makes_double/declares_dry_class.rb
+++ lib/mocktail/imitates_type/makes_double/declares_dry_class.rb
@@ -3,6 +3,8 @@ module Mocktail
     def initialize
       @handles_dry_call = HandlesDryCall.new
       @raises_neato_no_method_error = RaisesNeatoNoMethodError.new
+      @transforms_params = TransformsParams.new
+      @builds_method_signature = BuildsMethodSignature.new
     end
 
     def declare(type, instance_methods)
@@ -41,19 +43,26 @@ module Mocktail
     def define_double_methods!(dry_class, type, instance_methods)
       handles_dry_call = @handles_dry_call
       instance_methods.each do |method|
-        dry_class.define_method method, ->(*args, **kwargs, &block) {
-          handles_dry_call.handle(Call.new(
-            singleton: false,
-            double: self,
-            original_type: type,
-            dry_type: dry_class,
-            method: method,
-            original_method: type.instance_method(method),
-            args: args,
-            kwargs: kwargs,
-            block: block
-          ))
-        }
+        parameters = type.instance_method(method).parameters
+        signature = @transforms_params.transform(Call.new, params: parameters)
+        method_signature = @builds_method_signature.build(signature)
+        # TODO: This is failing because none of self, type, dry_class, method,
+        # etc are in scope for the dry_class.
+        dry_class.class_eval <<-RUBY, __FILE__, __LINE__ + 1
+          def #{method}#{method_signature}
+            HandlesDryClass.new.handle(Call.new(
+              singleton: false,
+              double: self,
+              original_type: type,
+              dry_type: dry_class,
+              method: method,
+              original_method: type.instance_method(method),
+              args: args,
+              kwargs: kwargs,
+              block: block
+            ))
+          end
+        RUBY
       end
     end
 

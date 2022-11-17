0f307100098666b013979ca38f901a4981bd7c43
Justin Searls <searls@gmail.com>
2022-11-17 09:52:02 -0500

    start refactoring this logic -- just realized it introduces a bug I need to write a test for

--- lib/mocktail/imitates_type/makes_double/declares_dry_class/reconstructs_call.rb
+++ lib/mocktail/imitates_type/makes_double/declares_dry_class/reconstructs_call.rb
@@ -1,6 +1,6 @@
 module Mocktail
   class ReconstructsCall
-    def reconstruct(double:, default_args:, call_binding:, dry_class:, type:, method:, original_method:, signature:)
+    def reconstruct(double:, call_binding:, default_args:, dry_class:, type:, method:, original_method:, signature:)
       Call.new(
         singleton: false,
         double: double,
@@ -8,12 +8,22 @@ module Mocktail
         dry_type: dry_class,
         method: method,
         original_method: original_method,
-        args: signature.positional_params.allowed.reject { |p| default_args&.key?(p) }.map { |p| call_binding.local_variable_get(p) } +
-          ((call_binding.local_variable_get(signature.positional_params.rest) if signature.positional_params.rest && !default_args&.key?(signature.positional_params.rest)) || []),
-        kwargs: signature.keyword_params.allowed.reject { |p| default_args&.key?(p) }.to_h { |p| [p, call_binding.local_variable_get(p)] }.merge(
-          (call_binding.local_variable_get(signature.keyword_params.rest) if signature.keyword_params.rest && !default_args&.key?(signature.keyword_params.rest)) || {}
-        ),
+        args: args_for(signature, call_binding, default_args),
+        kwargs: kwargs_for(signature, call_binding, default_args),
         block: call_binding.local_variable_get(signature.block_param || ::Mocktail::Signature::DEFAULT_BLOCK_PARAM)
+      )
+    end
+
+    private
+
+    def args_for(signature, call_binding, default_args)
+      signature.positional_params.allowed.reject { |p| default_args&.key?(p) }.map { |p| call_binding.local_variable_get(p) } +
+        ((call_binding.local_variable_get(signature.positional_params.rest) if signature.positional_params.rest && !default_args&.key?(signature.positional_params.rest)) || [])
+    end
+
+    def kwargs_for(signature, call_binding, default_args)
+      signature.keyword_params.allowed.reject { |p| default_args&.key?(p) }.to_h { |p| [p, call_binding.local_variable_get(p)] }.merge(
+        (call_binding.local_variable_get(signature.keyword_params.rest) if signature.keyword_params.rest && !default_args&.key?(signature.keyword_params.rest)) || {}
       )
     end
   end

f1b97ad9291eb8496dc7d57937889e5f52cd7250
Justin Searls <searls@gmail.com>
2022-11-16 23:42:10 -0500

    remove unit-of-work/transactional ivars

--- lib/mocktail/builds_method_signature.rb
+++ lib/mocktail/builds_method_signature.rb
@@ -1,50 +1,57 @@
 module Mocktail
   class BuildsMethodSignature
     def build(signature)
-      @signature = signature
-      "(#{[positional, keyword, block, dotdotdot].reject(&:empty?).join(", ")})"
+      positional_params = positional(signature)
+      dotdotdot_param = dotdotdot(signature)
+      keyword_params = keyword(signature) if dotdotdot_param.nil?
+      block_param = block(signature) if dotdotdot_param.nil?
+
+      "(#{[positional_params, dotdotdot_param, keyword_params, block_param].compact.join(", ")})"
     end
 
-    def dotdotdot
-      @dotdotdot ||= if @signature.positional_params.rest == :* &&
-          @signature.keyword_params.rest == :** &&
-          @signature.block_param == :&
-        "..."
-      else
-        ""
-      end
-    end
+    private
 
-    def positional
-      @signature.positional_params.all.map do |name|
-        if @signature.positional_params.required.include?(name) ||
-            @signature.positional_params.optional.include?(name)
+    def positional(signature)
+      params = signature.positional_params.all.map do |name|
+        if signature.positional_params.allowed.include?(name)
           "#{name} = ((__mocktail_default_args ||= {})[:#{name}] = nil)"
-        elsif @signature.positional_params.rest == name && name != :*
+        elsif signature.positional_params.rest == name && name != :*
           "*#{name}"
         end
-      end.compact.join(", ")
+      end.compact
+
+      params.join(", ") if params.any?
     end
 
-    def keyword
-      if dotdotdot.empty?
-        @signature.keyword_params.all.map do |name|
-          if @signature.keyword_params.required.include?(name) ||
-              @signature.keyword_params.optional.include?(name)
-            "#{name}: ((__mocktail_default_args ||= {})[:#{name}] = nil)"
-          elsif @signature.keyword_params.rest == name && name != :**
-            "**#{name}" end
-        end.join(", ")
-      else
-        ""
+    def keyword(signature)
+      params = signature.keyword_params.all.map do |name|
+        if signature.keyword_params.allowed.include?(name)
+          "#{name}: ((__mocktail_default_args ||= {})[:#{name}] = nil)"
+        elsif signature.keyword_params.rest == name && name != :**
+          "**#{name}"
+        end
+      end.compact
+
+      params.join(", ") if params.any?
+    end
+
+    def block(signature)
+      if signature.block_param
+        "&#{signature.block_param}"
       end
     end
 
-    def block
-      if dotdotdot.empty? && @signature.block_param
-        "&#{@signature.block_param}"
-      else
-        ""
+    def dotdotdot(signature)
+      if signature.positional_params.rest == :* &&
+          signature.keyword_params.rest == :** &&
+          signature.block_param == :&
+        "..."
+      end
+    end
+
+    def rest_name(params)
+      if params.rest && params.rest != :* && params.rest != :**
+        params.rest
       end
     end
   end

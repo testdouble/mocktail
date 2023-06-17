63f0a2e2d0e4736fedebb35550b5f071df626562
Justin Searls <searls@gmail.com>
2023-06-17 10:45:32 -0400

    fix flake

--- test/safe/dsl_test.rb
+++ test/safe/dsl_test.rb
@@ -4,6 +4,10 @@ require "test_helper"
 
 class DslTest < Minitest::Test
   def test_that_stubs_and_verifies_have_matching_options
-    assert_equal Mocktail.method(:stubs).parameters, Mocktail.method(:verify).parameters
+    assert_equal unwrap(Mocktail.method(:stubs)).parameters, unwrap(Mocktail.method(:verify)).parameters
+  end
+
+  def unwrap(method)
+    T::Private::Methods.signature_for_method(method)
   end
 end

e2a13b135d95d79a11e2ac45e73b4bcc70df9ccc
Justin Searls <searls@gmail.com>
2023-06-28 16:28:38 -0600

    some tests

--- sub_projects/untyped_user/antitype_test.rb
+++ sub_projects/untyped_user/antitype_test.rb
@@ -17,12 +17,35 @@ class AntitypeTest < Minitest::Test
     assert_equal "things", stuff.things(1, 2, sandals: 3)
   end
 
+  class Stuffing
+    include Stuff
+  end
+
   def test_matchers
     stuff = Mocktail.of(Stuff)
 
     stubs { stuff.things(:zubon, :shatsu, sandals: false) }.with { "things" }
+    stubs { |m| stuff.things(m.numeric, m.is_a(Stuff), sandals: m.includes(true)) }.with { "crap" }
 
     assert_equal "things", stuff.things(:zubon, :shatsu, sandals: false)
+    assert_equal "crap", stuff.things(42, Stuffing.new, sandals: [true, false])
+    assert_nil stuff.things(42, Stuffing.new, sandals: ["true"])
+    assert_nil stuff.things("42", Stuffing.new, sandals: [true])
+    assert_nil stuff.things(42, Stuffing, sandals: [true])
+
+    stuff.things(:a, :b, sandals: :c)
+    stuff.things(:c, :d, sandals: :e)
+    stuff.things(:f, :g, sandals: :h)
+
+    captor = Mocktail.captor
+    verify { stuff.things(:c, captor.capture, sandals: :e) }
+    assert_equal captor.value, :d
+  end
+
+  def test_sorbet_runtime_is_not_loaded
+    refute $LOADED_FEATURES.any? { |feature|
+      feature.include?("sorbet-runtime")
+    }, "sorbet-runtime was required by something in mocktail. Features:\n\n#{$LOADED_FEATURES.join("\n")}"
   end
 
   def teardown

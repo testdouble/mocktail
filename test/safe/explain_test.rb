ab950cd40f0d076249a567cf3659d9800ce5243e
Ross Brandes <ross@testdouble.com>
2026-09-18 14:28:50 -0400

    Track Ruby's native Hash#inspect format in different versions
    Add test/support/version_helper.rb (ported from #26) as a shared
    place for that Ruby-version check, and use it in both
    stringifies_call_test.rb and explain_test.rb.

--- test/safe/explain_test.rb
+++ test/safe/explain_test.rb
@@ -1,10 +1,12 @@
 # typed: strict
 
 require "test_helper"
+require "support/version_helper"
 
 class ExplainTest < Minitest::Test
   include Mocktail::DSL
   extend T::Sig
+  include VersionHelper
 
   class Thing
     extend T::Sig
@@ -41,7 +43,7 @@ class ExplainTest < Minitest::Test
 
       The call site:
 
-        #{__FILE__}:25:in #{call_site_method("ExplainTest#test_explain_stub_returned_nil")}
+        #{__FILE__}:27:in #{at_least_ruby_3_4? ? "'ExplainTest#" : "`"}test_explain_stub_returned_nil'
 
       No stubbings were configured on this method.
 
@@ -75,7 +77,7 @@ class ExplainTest < Minitest::Test
 
       The call site:
 
-        #{__FILE__}:56:in #{call_site_method("ExplainTest#test_explain_stub_returned_nil_with_stubbings")}
+        #{__FILE__}:58:in #{at_least_ruby_3_4? ? "'ExplainTest#" : "`"}test_explain_stub_returned_nil_with_stubbings'
 
       Stubbings configured prior to this call but not satisfied by it:
 
@@ -247,18 +249,6 @@ class ExplainTest < Minitest::Test
     ref = result.reference
     if ref.is_a?(Mocktail::FakeMethodData)
       assert_equal Training, ref.receiver
-    end
-  end
-
-  private
-
-  # Ruby 3.4 changed backtrace lines from `` `method' `` to `` 'Class#method' ``.
-  sig { params(qualified_method: String).returns(String) }
-  def call_site_method(qualified_method)
-    if Gem::Version.new(RUBY_VERSION) < Gem::Version.new("3.4")
-      "`#{qualified_method.split("#").last}'"
-    else
-      "'#{qualified_method}'"
     end
   end
 end
